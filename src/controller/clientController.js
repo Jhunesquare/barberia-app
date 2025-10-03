e24c42de82169fdbeede7ce116d5e3614d899415
Victor Hugo Soto <victorhsoto07@gmail.com>
2025-10-03 17:12:15 -0500

    Corrección del clientController

--- src/controller/clientController.js
+++ src/controller/clientController.js
@@ -1,9 +1,9 @@
 const clientServices = require('../services/client.service');
 
-const createUser = async(req, res) =>{
+const createUser = async (req, res) => {
     const data = req.body;
     //console.log("Email: ", data.correo);
-    if(!data.correo || !data.contraseña || !data.nombre || !data.apellido || !data.celular){
+    if (!data.correo || !data.contraseña || !data.nombre || !data.apellido || !data.celular) {
         return res.status(400).json({
             msg: 'error de credenciales'
         });
@@ -13,28 +13,7 @@ const createUser = async(req, res) =>{
 
 const getAll = async (req, res) => {
 
-    try {
-        const { authorization } = req.headers;
-        const auth = verifyToken(authorization);
-
-        if (!auth.status) {
-            return res.status(auth.statusCode).json({
-                msg: auth.msg
-            });
-        } else if (auth.decoded.rol != 1) {
-            return res.status(401).json({
-                msg: 'unauthorized'
-            });
-        }
-
-        await services.getAllService(req, res);
-
-    } catch (e) {
-        res.status(401).json({
-            msg: 'unauthorized',
-            error: e
-        });
-    }
+    await services.getAllService(req, res);
 
 }
 
@@ -48,4 +27,4 @@ const getById = async (req, res) => {
     await clientServices.getByIdService(data, res);
 }
 
-module.exports = {createUser, getAll, getById};
+module.exports = { createUser, getAll, getById };
